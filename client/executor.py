8f3f79118c1a1731431a479588cb7969963219b8
Dušan Lazić <lazicdusan@protonmail.com>
2024-07-05 17:02:48 +0200

    Write executor

--- client/executor.py
+++ client/executor.py
@@ -1,13 +1,41 @@
+import re
+import os
+import sys
 import argparse
+import concurrent.futures
+from typing import Callable, Any
+from importlib import import_module
 from shared.logs import logger
+
+FLAG_REGEX = "ENO[A-Za-z0-9+\/=]{48}"  # TODO: Propagate from server to here
 
 
 def main(args):
-    logger.debug(f"Running exploit {args.name} against {args.targets}.")
-    import time
+    exploit = import_exploit_func(args.module)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(exploit, target) for target in args.targets]
 
-    time.sleep(1)
-    logger.debug("Attacks finished.")
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            flags = match_flags(FLAG_REGEX, result)
+            # TODO: Enqueue flags
+            logger.success(flags)
+
+
+def import_exploit_func(module) -> Callable[[str], Any]:
+    cwd = os.getcwd()
+    if cwd not in sys.path:
+        sys.path.append(cwd)
+
+    module = import_module(module)
+    exploit_func = getattr(module, "exploit", None)
+
+    return exploit_func
+
+
+def match_flags(pattern: str, text: str) -> bool:
+    matches = re.findall(pattern, text)
+    return matches if matches else None
 
 
 if __name__ == "__main__":
@@ -34,7 +62,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--timeout",
-        default=30,
+        default=10,
         type=str,
         required=True,
         help="Optional timeout for a single attack in seconds.",
